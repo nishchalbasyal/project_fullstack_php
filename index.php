65c2d45ef897514151546ba81acae5fe63840e28
Nishchal Basyal <nischalbasyal2003@gmail.com>
2025-02-23 16:23:00 +0545

    ReadmeFile Added

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Welcome To the Our Website</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css">
 
